f686ec948d7ed8736daa84e276a8c2432ae35e7f
Lucas Caro <lucascaro@gmail.com>
2018-10-13 00:05:10 -0700

    explicit ignore and include path for tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [
-    "**/__tests__/*.+(ts|tsx|js)"
+    "<rootDir>/src/**/__tests__/*.+(ts|tsx|js)"
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/.*'
   ],
   collectCoverage: true,
 };
