a343caff50b0907649ddb760cb0092d649aa585f
Lucas Caro <lucascaro@gmail.com>
2019-09-10 10:18:52 -0700

    update with badge

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 VERSION=$(git describe --tags --abbrev=0)
 
 rm -rf versions/$VERSION && mkdir -p versions/$VERSION
-cp -r docs/* $VERSION
+cp -r docs/* versions/$VERSION
 rm -rf assets enums interfaces modules *.html
 cp -r docs/* .
 
