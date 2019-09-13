90e6980d0e91eba90ca07694fceca894b9b34a22
Lucas Caro <lucascaro@gmail.com>
2019-09-13 08:53:30 -0700

    add new version docs

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -ex
 
-VERSION=$(git describe --tags --abbrev=0)
+VERSION=$(ls docs/versions/)
 
-rm -rf versions/$VERSION && mkdir -p versions/$VERSION
-cp -r docs/* versions/$VERSION
-rm -rf assets enums interfaces modules *.html
+rm -rf assets enums interfaces modules *.html versions/$VERSION
 cp -r docs/* .
 
